0a6a9e4bd99412bb367f59d60c225b531225b3d0
Abdlrhmn-Morsi <abdlrhmnmorsi@gmail.com>
2022-10-09 23:22:15 +0200

    updated

--- lib/const/colors.dart
+++ lib/const/colors.dart
@@ -4,6 +4,6 @@ abstract class MyConst {
   static const darkBlue = Color.fromARGB(255, 0, 10, 24);
   static const liteBlue = Color.fromARGB(255, 1, 31, 77);
   static const blue = Color.fromRGBO(1, 19, 46, 1);
-  static const apiKey = 'ADD_YOUR_API_KEY';
+  static const apiKey = 'apiKey=984005c37e8847f49f9ceaa50f3479b8';
 }
 
